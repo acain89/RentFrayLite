f4f61dee048c048cb1b351d1a4d8b06eef20df81
Andrew Cain <acain89@gmail.com>
2026-07-15 13:26:19 -0500

    Remove obsolete PWA service worker reference

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import PwaServiceWorker from "./components/PwaServiceWorker";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <PwaServiceWorker />
         {children}
       </body>
     </html>
